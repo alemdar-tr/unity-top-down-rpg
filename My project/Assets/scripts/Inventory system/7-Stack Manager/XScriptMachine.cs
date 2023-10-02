e52767536378f6a0adc959e6718169cd610b9ee8
jvchgaming <jordyveermanc@gmail.com>
2023-10-02 21:30:31 +0200

    tiny changes

---
+++ My project/Assets/scripts/Inventory system/7-Stack Manager/XScriptMachine.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.VisualScripting;
+
+[IncludeInSettings(true)]
+public static class XScriptMachine
+{
+    public static void InstantiateMachine(GameObject target, ScriptGraphAsset macro)
+    {
+        target.AddComponent<ScriptMachine>().nest.SwitchToMacro(macro);
+    }
+
+}
